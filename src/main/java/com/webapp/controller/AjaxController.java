16b398a98fd7f9487cbcdfb17584e6de6c64d108
webappcar <chaoslab@naver.com>
2015-09-22 16:02:50 +0900

    1st add

---
+++ src/main/java/com/webapp/controller/AjaxController.java
@@ -0,0 +1,18 @@
+package com.webapp.controller;
+
+import org.springframework.stereotype.Controller;
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RequestMethod;
+
+@Controller
+@RequestMapping("/ajax")
+public class AjaxController {
+	
+	@RequestMapping(value="/view", method=RequestMethod.GET)
+	public String getView() {
+		System.out.println("test!!");
+		
+		return "ajax/view";
+	}
+
+}
---
+++ src/main/java/com/webapp/mapper/Mapper.java
@@ -0,0 +1,5 @@
+package com.webapp.mapper;
+
+public interface Mapper {
+
+}
