1b19387ac513457575e9231222e78d9c1cf5d040
webappcar <chaoslab@naver.com>
2015-09-24 16:04:34 +0900

    ad qa controller

---
+++ src/main/java/com/shoppingmall/mapper/QAMapper.java
@@ -0,0 +1,24 @@
+package com.shoppingmall.mapper;
+
+public interface QAMapper {
+
+	public int getListSize();		
+	public int getPageNum();
+	public void setPageNum(int pageNum);
+	
+	public boolean isFirstPage();
+	public void setFirstPage(boolean firstPage);
+	public boolean isLastPage();
+	public void setLastPage(boolean lastPage);
+	
+	public Integer[] getQaNoList();
+	public String[] getQaTitleList();
+	public String[] getQaWriterList();
+	public String[] getQaRDateList();	
+	
+	public void setQaNoList(int index, Integer qaNoList);
+	public void setQaTitleList(int index, String qaTitleList);
+	public void setQaWriterList(int index, String qaWriterList);
+	public void setQaRDateList(int index, String qaRDateList);
+	
+}
